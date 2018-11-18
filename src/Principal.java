83fc5a6c710fddbf1cf736929d35d12b7b39a3b9
levig <levig@DESKTOP-SHORE01>
2018-11-18 13:35:38 -0300

    Pedra movendo

--- src/Principal.java
+++ src/Principal.java
@@ -4,24 +4,41 @@ import java.awt.event.KeyListener;
 import static java.lang.Thread.sleep;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
-public class Principal extends JFrame {
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Principal extends JFrame implements Runnable{
 	
 	Desenho bcgrnd = new Desenho("background.gif", 498, 720);
 	
 	JLabel lBcgrnd = new JLabel(bcgrnd.getImg());
 	
-	DesenhoMovel stone1 = new DesenhoMovel("stone1.png", 256, 256);	
-	JLabel lStone1 = new JLabel(stone1.getImg());
-	DesenhoMovel stone2 = new DesenhoMovel("stone2.png", 256, 256);	
-	JLabel lStone2 = new JLabel(stone2.getImg());
-	DesenhoMovel stone3 = new DesenhoMovel("stone3.png", 256, 256);	
-	JLabel lStone3 = new JLabel(stone3.getImg());
+	ArrayList<DesenhoMovel> stones = new ArrayList<DesenhoMovel>();
 	
-	DesenhoMovel ship1 = new DesenhoMovel("ship1.png", 128, 128);
+	
+	boolean stone1 = stones.add(new DesenhoMovel("stone1.png", ThreadLocalRandom.current().nextInt(0, 418), 10));	
+	JLabel lStone1 = new JLabel(stones.get(0).getImg());
+//	DesenhoMovel stone2 = new DesenhoMovel("stone2.png", 256, 256);	
+//	JLabel lStone2 = new JLabel(stone2.getImg());
+//	DesenhoMovel stone3 = new DesenhoMovel("stone3.png", 256, 256);	
+//	JLabel lStone3 = new JLabel(stone3.getImg());
+	
+	DesenhoMovel ship1 = new DesenhoMovel("ship1.png", 188, 568);
 	JLabel lShip1 = new JLabel(ship1.getImg());
 	
+	public void run() {
+		while (true) {
+			try {
+				sleep(190);
+				stones.get(0).moveDown();
+				lStone1.setBounds(stones.get(0).getX(), stones.get(0).getY(), lStone1.getWidth(), lStone1.getHeight());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
 	public Principal() {
 	    this.window();
 	    this.component();
@@ -29,38 +46,39 @@ public class Principal extends JFrame {
 	    //new Veficacao().start();
     }
 	
-	public void moveRight(DesenhoMovel d) {
-		if(ship1.getX() < 388) {
-			ship1.moveRight();
-			System.out.println(ship1.getX());
-			lShip1.setBounds(ship1.getX(), ship1.getY(), lShip1.getWidth(), lShip1.getHeight());
+	public void moveRight(DesenhoMovel d, JLabel e) {
+		if(d.getX() < 388) {
+			d.moveRight();
+			e.setBounds(d.getX(), d.getY(), e.getWidth(), e.getHeight());
 		}
 	}
 	
-	public void moveUp(DesenhoMovel d) {
-		if(ship1.getY() > 10) {
-			ship1.moveUp();
-			lShip1.setBounds(ship1.getX(), ship1.getY(), lShip1.getWidth(), lShip1.getHeight());
+	public void moveUp(DesenhoMovel d, JLabel e) {
+		if(d.getY() > 10) {
+			d.moveUp();
+			e.setBounds(d.getX(), d.getY(), e.getWidth(), e.getHeight());
 		}
 	}
 	
-	public void moveLeft(DesenhoMovel d) {
-		if(ship1.getX() > 0) {
-			ship1.moveLeft();
-			lShip1.setBounds(ship1.getX(), ship1.getY(), lShip1.getWidth(), lShip1.getHeight());
+	public void moveLeft(DesenhoMovel d, JLabel e) {
+		if(d.getX() > 0) {
+			d.moveLeft();
+			e.setBounds(d.getX(), d.getY(), e.getWidth(), e.getHeight());
 		}
 	}
 	
-	public void moveDown(DesenhoMovel d) {
-		if(ship1.getY() < 568) {
-			ship1.moveDown();
-			lShip1.setBounds(ship1.getX(), ship1.getY(), lShip1.getWidth(), lShip1.getHeight());
+	public void moveDown(DesenhoMovel d, JLabel e) {
+		if(d.getY() < 568) {
+			d.moveDown();
+			e.setBounds(d.getX(), d.getY(), e.getWidth(), e.getHeight());
 		}
 	}
 	
 	
 	public static void main(String[] args) {
-		Principal app = new Principal();
+		Runnable app = new Principal();
+		Thread app0 = new Thread(app);
+		app0.start();
 	}
 	public void addMoviment() {
 		addKeyListener(new KeyListener() {
@@ -70,19 +88,21 @@ public class Principal extends JFrame {
 			
 			@Override
 			public void keyReleased(KeyEvent arg0) {
-				if(arg0.getKeyCode() == 38) {
-					moveUp(ship1);
-				} else if(arg0.getKeyCode() == 40){
-					moveDown(ship1);
-				} else if(arg0.getKeyCode() == 39){
-					moveRight(ship1);
-				} else if(arg0.getKeyCode() == 37){
-					moveLeft(ship1);
-				}
+				
 			}
 			
 			@Override
-			public void keyPressed(KeyEvent arg0) {}
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == 38) {
+				moveUp(ship1, lShip1);
+				} else if(arg0.getKeyCode() == 40){
+					moveDown(ship1, lShip1);
+				} else if(arg0.getKeyCode() == 39){
+					moveRight(ship1, lShip1);
+				} else if(arg0.getKeyCode() == 37){
+					moveLeft(ship1, lShip1);
+				}
+			}
 		});
 	}
 	public void window() {
@@ -92,7 +112,7 @@ public class Principal extends JFrame {
         this.setVisible(true);
         setResizable(false);
         
-//        this.add(lStone1);
+        this.add(lStone1);
 //        this.add(lStone2);
 //        this.add(lStone3);
         this.add(lShip1);
@@ -101,7 +121,7 @@ public class Principal extends JFrame {
 	public void component() {
 		lBcgrnd.setBounds(0, 0, 498, 720);
 		lShip1.setBounds(ship1.getX(), ship1.getY(), 128, 128);
-//		lStone1.setBounds(stone1.getX(), stone1.getY(), 80, 60);
+		lStone1.setBounds(stones.get(0).getX(), stones.get(0).getY(), 80, 60);
 //		lStone2.setBounds(stone2.getX(), stone2.getY(), 40, 60);
 //		lStone3.setBounds(stone3.getX(), stone3.getY(), 10, 60);
 	}
